eab985f82c2ee9aff73a5bf1e9180deeb891cf06
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-19 20:55:54 -0300

    Criando adapter customizado para lista de tarefas

---
+++ app/src/main/java/br/com/matheush/apptodolist/adapter/TarefaAdapter.java
@@ -0,0 +1,95 @@
+package br.com.matheush.apptodolist.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.matheush.apptodolist.R;
+import br.com.matheush.apptodolist.model.Tarefa;
+
+/**
+ * Created by matheush on 18/02/17.
+ */
+
+public class TarefaAdapter extends RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder> {
+    private Context context;
+    private List<Tarefa> tarefaList;
+    private OnItemClickListener onItemClicked;
+    private OnItemLongClickListener onItemLongClicked;
+
+    public TarefaAdapter(Context context, List<Tarefa> tarefaList, OnItemClickListener onItemClicked, OnItemLongClickListener onItemLongClicked) {
+        this.context = context;
+        this.tarefaList = tarefaList;
+        this.onItemClicked = onItemClicked;
+        this.onItemLongClicked = onItemLongClicked;
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.tarefaList != null ? this.tarefaList.size() : 0;
+    }
+
+    @Override
+    public TarefaViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.content_tarefa_row, viewGroup, false);
+
+        TarefaViewHolder holder = new TarefaViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final TarefaViewHolder holder, final int position) {
+        //Atualiza a view
+        Tarefa t = tarefaList.get(position);
+
+        holder.tvTarefa.setText(t.getTarefa());
+        holder.tvDataHora.setText(t.getDataHoraString());
+
+        //Click
+        if (onItemClicked != null) {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClicked.onItemClicked(holder.itemView, position);
+                }
+            });
+        }
+
+        //Long press
+        if (onItemLongClicked != null) {
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    onItemLongClicked.onItemLongClicked(position);
+                    return true;
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        public void onItemClicked(View view, int position);
+    }
+
+    public interface OnItemLongClickListener {
+        public boolean onItemLongClicked(int position);
+    }
+
+    public class TarefaViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTarefa;
+        TextView tvDataHora;
+
+        public TarefaViewHolder(View view) {
+            super(view);
+
+            tvTarefa = (TextView) view.findViewById(R.id.card_tv_tarefa);
+            tvDataHora = (TextView) view.findViewById(R.id.card_tv_data_hora);
+        }
+    }
+}
