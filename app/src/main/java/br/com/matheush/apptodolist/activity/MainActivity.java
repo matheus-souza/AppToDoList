100acb81a9963cb18ae3244c6e2b09ab1fa7a664
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-18 22:32:26 -0300

    Implementando lista de tarefas no layout

--- app/src/main/java/br/com/matheush/apptodolist/activity/MainActivity.java
+++ app/src/main/java/br/com/matheush/apptodolist/activity/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_excluir_todos) {
             return true;
         }
 
