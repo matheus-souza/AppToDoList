e7ac0c374a84ffaf562ac23abac043913bd9e153
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-02-21 00:34:29 -0300

    Implementando lista de tarefas

--- app/src/main/java/br/com/matheush/apptodolist/activity/MainActivity.java
+++ app/src/main/java/br/com/matheush/apptodolist/activity/MainActivity.java
@@ -3,20 +3,47 @@ package br.com.matheush.apptodolist.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.matheush.apptodolist.MyApplication;
 import br.com.matheush.apptodolist.R;
+import br.com.matheush.apptodolist.adapter.TarefaAdapter;
+import br.com.matheush.apptodolist.model.Tarefa;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmResults;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Validator.ValidationListener {
 
     @BindView(R.id.fab)
     FloatingActionButton fab;
+    @NotEmpty(message = MyApplication.MSG_VAZIO)
+    @BindView(R.id.et_nova_tarefa_main)
+    EditText etNovaTarefa;
+    @BindView(R.id.rv_tarefas_main)
+    RecyclerView rvTarefas;
+
     private TarefaAdapter tarefaAdapter;
+    private Validator validator;
+    private List<Tarefa> tarefaList;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +53,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+        tarefaList = new LinkedList<>();
+
+        Realm.init(getApplicationContext());
+        realm = Realm.getDefaultInstance();
+
+        rvTarefas.setLayoutManager(new LinearLayoutManager(this));
+        rvTarefas.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
+        rvTarefas.setItemAnimator(new DefaultItemAnimator());
+        rvTarefas.setHasFixedSize(true);
+
+        tarefaAdapter = new TarefaAdapter(MainActivity.this, tarefaList, onItemClickListener(), onItemLongClickListener());
+        rvTarefas.setAdapter(tarefaAdapter);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //ADICIONA A ATIVIDADE
+                validator.validate();
             }
         });
     }
 
+    public void atualizaLista() {
+        RealmResults<Tarefa> tarefas = realm.where(Tarefa.class).findAll();
+        tarefaList.clear();
+        tarefaList.addAll(tarefas);
+        tarefaAdapter.notifyDataSetChanged();
+    }
+
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        atualizaLista();
+    }
+
     protected TarefaAdapter.OnItemClickListener onItemClickListener() {
         return new TarefaAdapter.OnItemClickListener() {
             @Override
@@ -53,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -72,5 +129,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
