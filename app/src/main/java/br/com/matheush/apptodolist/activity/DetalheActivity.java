481aeda5a8d29604c40d92ffe08fdeaedcdb78ef
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-03-02 00:38:03 -0300

    Criando activity de visualização de detalhes das tarefas

---
+++ app/src/main/java/br/com/matheush/apptodolist/activity/DetalheActivity.java
@@ -0,0 +1,29 @@
+package br.com.matheush.apptodolist.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import br.com.matheush.apptodolist.R;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    public static final String TAG = "LogX_Detalhe";
+
+    long index;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        index = intent.getLongExtra("index", 0);;
+        Log.d(TAG, "id: " + index);
+    }
+
+}
