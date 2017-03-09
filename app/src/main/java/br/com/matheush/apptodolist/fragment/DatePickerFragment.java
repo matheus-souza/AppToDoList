4da6dcda16e86fd139c61df594ba304fa1fb3b82
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-03-09 00:53:44 -0300

    Criando gragmento para a seleção da data

---
+++ app/src/main/java/br/com/matheush/apptodolist/fragment/DatePickerFragment.java
@@ -0,0 +1,42 @@
+package br.com.matheush.apptodolist.fragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+import br.com.matheush.apptodolist.R;
+
+/**
+ * Created by matheush on 09/03/17.
+ */
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //Use the current date as the default date in the date picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        //Create a new DatePickerDialog instance and return it
+        /*
+            DatePickerDialog Public Constructors - Here we uses first one
+            public DatePickerDialog (Context context, DatePickerDialog.OnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth)
+            public DatePickerDialog (Context context, int theme, DatePickerDialog.OnDateSetListener listener, int year, int monthOfYear, int dayOfMonth)
+         */
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        EditText etData = (EditText) getActivity().findViewById(R.id.edicao_et_data);
+
+        etData.setText(dayOfMonth + "/" + month + "/" + year);
+    }
+}
